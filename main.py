2e5fd4fe00ff1a1b00a1a55122491bb8427b5f1f
diegoospina9711 <diego_ale.ospina@uao.edu.co>
2024-08-19 14:41:11 -0500

    Interfaz EIS

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-#  ejemplo
+#  Diego Alejandro Ospina Ceron 
+#   Programa principal para interfaz EIS
+#   utilizando python
+from eis import Ui
+import sys
+from PyQt5 import QtWidgets
+
+if __name__ == "__main__":    
+    app = QtWidgets.QApplication(sys.argv)
+    window = Ui()
+    sys.exit(app.exec_())
